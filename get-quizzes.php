a4e4b6cbb25d30d9fab765d96a5befb7583920f8
faisal <talebfaisal1@gmail.com>
2025-04-27 15:05:23 +0300

    Added API to retrieve all quizzes

---
+++ get-quizzes.php
@@ -0,0 +1,14 @@
+<?php
+include 'db.php';
+header('Content-Type: application/json');
+
+$sql = "SELECT * FROM quizzes";
+$result = $conn->query($sql);
+
+$quizzes = [];
+while ($row = $result->fetch_assoc()) {
+    $quizzes[] = $row;
+}
+
+echo json_encode($quizzes);
+?>
